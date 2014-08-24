170dff0060752bbc3d71f813767fe71d23fe5df4
myidealab <myidealab@users.noreply.github.com>
2014-08-24 17:40:43 -0400

    makeCacheMatrix and cacheSolve functions have been created for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+##  Function 1: "makeCacheMatrix" is a function that makes a 
+##  matrix object, whose inverse can be cached.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## The input for x is a matrix.
+    m <- NULL                               ## m stores the matrix; it is 
+                                            ## assigned the value of NULL.
+    
+    set <- function(y) {                    ## The set function sets the value of the matrix.
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x                     ## The get function gets the value of the original matrix.
+    setmtrx <- function(mtrx) m <<- mtrx    ## `<<-` operator is used to assign the cacheSolve() value.
+    getmtrx <- function() m                 ## The cached value is retrieved.
+    list(set = set, get = get,
+         setmtrx = setmtrx,
+         getmtrx = getmtrx)
 }
 
+##  Function 2: "cacheSolve" is a function, that can calculate
+##  the inverse of the matrix object, which was created by the 
+##  makeCacheMatrix function.
+##  If the inverse already exists, then the cacheSolve function 
+##  will retrieve it from cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {            ## The input for x is a matrix.
+    m <- x$getmtrx()                        ## m stores the value of the inverse matrix.
+    if(!is.null(m)) {                       ## Conditional statement check if it exists.
+        message("getting cached data")      ## A message is displayed if it exists.
+        return(m)                           ## The value of the inverse matrix is returned,
+                                            ## if it exists.
+    }
+    data <- x$get()                         ## If the inverse matrix doesn't exist,                
+    m <- solve(data, ...)                   ## calculate the inverse matrix using the solve()
+    x$setmtrx(m)                            ## function.
+    m                                       ## Get the value of the inverse matrix.
 }
+
+## Check work
+n <- makeCacheMatrix(matrix(1:4, 2, 2))     ## Input test values for the matrix function
+n$get()
+
+cacheSolve(n)   # 1st try
+cacheSolve(n)   # 2nd try should get from cached data
+
+# Verify results using an matrix calculator
+# http://www.bluebit.gr/matrix-calculator/calculate.aspx
